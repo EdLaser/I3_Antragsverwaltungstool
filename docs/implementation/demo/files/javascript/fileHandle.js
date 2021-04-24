0ecfab44ffac40a850f67edd98c62c01b707115b
Ruben-David Kraus <s80350@htw-dresden.de>
2021-04-24 18:52:15 +0200

    Implementation erstellt

---
+++ docs/implementation/demo/files/javascript/fileHandle.js
@@ -0,0 +1,52 @@
+/*dropArea.addEventListener('dragover', (event) => {
+    //Prevent Browser from default actions -> dont open files in new window
+    event.stopPropagation();
+    event.preventDefault();
+
+    event.dataTransfer.dropEffect = 'copy';
+});*/
+/*dropArea.addEventListener('drop', (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const fileList = event.dataTransfer.files; //Get file list
+    console.log(fileList);                     //Print file log
+});*/
+//dropArea.addEventListener('drop', (event) => { ...
+// equals: dropArea.addEventlistener('drop', function(event) { ...
+/*function readFile(file) {
+    //Check if file is not pdf
+    if (file.type && !file.type.startsWith('pdf/')) {
+        console.log(file, 'Is not supported');
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.addEventListener('load',(event)=> {
+        file.src = event.target.result;
+        reader.readAsText(file);
+    });
+}*/
+/*function readFile() {
+    let reader = new FileReader();
+    reader.addEventListener('load', function (event) {
+        let text = event.target.result;
+        document.getElementById('output').textContent = text;
+    });
+}
+const fileArea = document.getElementById('#filezone');
+fileArea.addEventListener('change', readFile);*/
+
+document.getElementById('filezone').addEventListener('change',function fileSelected(){
+    console.log(this.files);
+    let file = this.files[0];
+
+    if ( file.size===0) {
+        console.log('No file');
+        return
+    }
+    let reader = new FileReader();
+    reader.onload = function (progressEvent) {
+        console.log(reader.result;
+    };
+    document.getElementById('out').innerHTML = this.files;
+});
