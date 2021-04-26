d4cb8ed5999d1f99952c4fa58331a2d5bba368ad
Ruben-David Kraus <s80350@htw-dresden.de>
2021-04-26 16:53:49 +0200

    feature(Dateiarbeit): Erste PHP Datei [Starts #8]

---
+++ docs/implementation/demo/files/php/create_form.php
@@ -0,0 +1,11 @@
+<?php
+   $titel = $_POST['antrag_titel'];
+   $vorname = $_POST['antrag_vorname'];
+   $nachname = $_POST['antrag_nachname'];
+   $email = $_POST['antrag_email'];
+   $datum = $_POST['antrag_datum'];
+   $text = $_POST['antrag_text'];
+   $grund = $_POST['antrag_grund'];
+   $vorschlag = $_POST['antrag_vorschlag'];
+   $anlagen = $_POST['antrag_anlagen'];
+?>
