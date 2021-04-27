dbac8be95d4ae3890a6ce1535ff3d37f75ed20cd
Ruben Kraus <s80350@gmail.com>
2021-04-27 11:47:15 +0200

    feature(DateiArbeit): PHP vervollständigt [Delivers #8]

--- docs/implementation/demo/files/php/create_form.php
+++ docs/implementation/demo/files/php/create_form.php
@@ -8,4 +8,6 @@
    $grund = $_POST['antrag_grund'];
    $vorschlag = $_POST['antrag_vorschlag'];
    $anlagen = $_POST['antrag_anlagen'];
+
+   exec("/Users/ruben/Library/Mobile Documents/com~apple~CloudDocs/Documents/Uni Online/I3_Antragsverwaltungstool/docs/implementation/demo/files/python/create_form.py $titel $vorname $nachname $email $datum $text $grund $vorschlag $anlagen")
 ?>
