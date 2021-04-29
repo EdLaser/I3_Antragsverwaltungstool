b875645f4696607a499a6b0f011d51b128e2bb02
Ruben Kraus <s80350@htw-dresden.de>
2021-04-29 18:19:00 +0200

    feature(DateiArbeit): Kommentare zum Testen [Delivers #8]

--- docs/implementation/demo/files/python/create_form.py
+++ docs/implementation/demo/files/python/create_form.py
@@ -5,7 +5,7 @@ print('Variable count: ', len(sys.argv))
 if len(sys.argv) == 0:
     print('failure')
 
-with open('new_form.txt', 'w') as write_file:
-    writer = csv.writer(write_file, delimiter=';')
-    for arg in sys.argv:
-        writer.writerow(arg)
+#with open('new_form.txt', 'w') as write_file:
+#    writer = csv.writer(write_file, delimiter=';')
+#    for arg in sys.argv:
+#        writer.writerow(arg)
