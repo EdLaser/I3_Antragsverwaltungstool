90a4ab248d33797dc4ebcf2cde187bd0bc3c2599
Ruben Kraus <s80350@htw-dresden.de>
2021-05-05 17:55:03 +0200

    feature(DateiArbeit):PHP-Datei angepast [Delivers #8]

--- docs/implementation/demo/files/php/create_form.php
+++ docs/implementation/demo/files/php/create_form.php
@@ -1,13 +1,16 @@
 <?php
-   $titel = $_POST['antrag_titel'];
-   $vorname = $_POST['antrag_vorname'];
-   $nachname = $_POST['antrag_nachname'];
-   $email = $_POST['antrag_email'];
-   $datum = $_POST['antrag_datum'];
-   $text = $_POST['antrag_text'];
-   $grund = $_POST['antrag_grund'];
-   $vorschlag = $_POST['antrag_vorschlag'];
-   $anlagen = $_POST['antrag_anlagen'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $titel = $_POST["antrag_titel"];
+   $vorname = $_POST["antrag_vorname"];
+   $nachname = $_POST["antrag_nachname"];
+   $email = $_POST["antrag_email"];
+   //$datum = $_POST["antrag_datum"];
+   $text = $_POST["antrag_text"];
+   $grund = $_POST["antrag_grund"];
+   $vorschlag = $_POST["antrag_vorschlag"];
+   $anlagen = $_POST["antrag_anlagen"];
+   echo "$titel $vorname $nachname $email $text $grund $vorschlag $anlagen";
 
-   exec("/Users/ruben/Library/Mobile Documents/com~apple~CloudDocs/Documents/Uni Online/I3_Antragsverwaltungstool/docs/implementation/demo/files/python/create_form.py $titel $vorname $nachname $email $datum $text $grund $vorschlag $anlagen")
-?>
+   exec("python /Users/ruben/Library/Mobile Documents/com~apple~CloudDocs/Documents/Uni Online/I3_Antragsverwaltungstool/docs/implementation/demo/files/python/create_form.py $titel $vorname $nachname $email $text $grund $vorschlag $anlagen", $output);
+   print_r($output);
+}
