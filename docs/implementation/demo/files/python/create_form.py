b6c62152fb961d4233afed393a3be0f435d1cbaa
Ruben Kraus <s80350@htw-dresden.de>
2021-05-07 09:30:17 +0200

    Form zum Testen

--- docs/implementation/demo/files/python/create_form.py
+++ docs/implementation/demo/files/python/create_form.py
@@ -1,11 +1,19 @@
 import csv
 import sys
+import os
 
 print('Variable count: ', len(sys.argv))
 if len(sys.argv) == 0:
     print('failure')
 
-#with open('new_form.txt', 'w') as write_file:
-#    writer = csv.writer(write_file, delimiter=';')
-#    for arg in sys.argv:
-#        writer.writerow(arg)
+
+#def create_file(filename):
+# List of things to write
+# open the file
+file_write = open(os.path.join('output', "new_form.txt"), 'w')
+writer = csv.writer(file_write, delimiter=';')
+for arg in sys.argv[1]:
+    writer.writerow(arg)
+
+
+#create_file("new_form.txt")
