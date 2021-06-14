e3bdbc320bfbddeb2f103b6e39a547875bbd3483
Ruben Kraus <s80350@htw-dresden.de>
2021-06-14 10:11:16 +0200

    Added all applications and getting applications by categorie

--- docs/implementation/Django_test/atool/views.py
+++ docs/implementation/Django_test/atool/views.py
@@ -1,12 +1,7 @@
 import datetime
 from django.shortcuts import render
-
-from django.http import HttpResponse
-from django.template import loader
-from django.views.generic import CreateView
-from .forms import NewUniversallForm
-
 from atool.models import Universall, Finance, AdvisoryMember, Position
+from itertools import chain
 
 
 def index(request):
@@ -33,6 +28,7 @@ def intern(request):
     return render(request, 'stat_html/intern.html')
 
 
+# Add a new universall database entry
 def new_universall(request):
     if request.method == 'POST':
         print(request.POST)
@@ -49,3 +45,93 @@ def new_universall(request):
         new_uni = Universall(flag, number, date, title, office, name, mail, text, reason, suggestion)
         new_uni.save()
     return render(request, 'stat_html/universally.html')
+
+
+def new_finance(request):
+    if request.method == 'POST':
+        print(request.POST)
+        flag = 0
+        number = '2020-01-01'
+        date = datetime.date.today()
+        title = request.POST.get('titel')
+        office = request.POST.get('election_input')
+        name = request.POST.get('name')
+        mail = request.POST.get('mail')
+        text = request.POST.get('antrtext')
+        reason = request.POST.get('begzantr')
+        budget = request.POST.get('haushplan')
+        suggestion = request.POST.get('vrshzverf')
+        new_fin = Finance(flag, number, date, title, office, name, mail, text, reason, budget, suggestion)
+        new_fin.save()
+    return render(request, 'stat_html/finance.html')
+
+
+def new_advisory(request):
+    if request.method == 'POST':
+        print(request.POST)
+        flag = 0
+        number = '2020-01-01'
+        date = datetime.date.today()
+        title = request.POST.get('titel')
+        office = request.POST.get('election_input')
+        name = request.POST.get('name')
+        mail = request.POST.get('mail')
+        text = request.POST.get('antrtext')
+        frg1 = request.POST.get('frg1')
+        frg2 = request.POST.get('frg2')
+        frg3 = request.POST.get('frg3')
+        frg4 = request.POST.get('frg4')
+        new_adv = AdvisoryMember(flag, number, date, title, office, name, mail, text, frg1, frg2, frg3, frg4)
+        new_adv.save()
+    return render(request, 'stat_html/advisory_member.html')
+
+
+def new_position(request):
+    if request.method == 'POST':
+        print(request.POST)
+        flag = 0
+        number = '2020-01-01'
+        date = datetime.date.today()
+        title = request.POST.get('titel')
+        office = request.POST.get('election_input')
+        name = request.POST.get('name')
+        mail = request.POST.get('mail')
+        text = request.POST.get('antrtext')
+        frg1 = request.POST.get('frg1')
+        frg2 = request.POST.get('frg2')
+        frg3 = request.POST.get('frg3')
+        frg4 = request.POST.get('frg4')
+        frg_spez_1 = request.POST.get('frg5')
+        frg_spez_2 = request.POST.get('frg6')
+        frg_spez_3 = request.POST.get('frg7')
+        new_pos = Position(flag, number, date, title, office, name, mail, text, frg1, frg2, frg3, frg4, frg_spez_1,
+                           frg_spez_2, frg_spez_3)
+        new_pos.save()
+    return render(request, 'stat_html/election_report.html')
+
+
+def get_universall(request):
+    if request.method == 'POST':
+        print(request)
+        uni_objects = Universall.objects.all().filter(office=request.POST.get('election_input'))
+        fin_objects = Finance.objects.all().filter(office=request.POST.get('election_input'))
+        pos_objects = Position.objects.all().filter(office=request.POST.get('election_input'))
+        adv_members = AdvisoryMember.objects.all().filter(office=request.POST.get('election_input'))
+        if request.POST.get('election_input') == '1':
+            office = 'Präsidium'
+        if request.POST.get('election_input') == '2':
+            office = 'Plenum'
+        if request.POST.get('election_input') == '3':
+            office = 'Qualitätsmanagement'
+        if request.POST.get('election_input') == '4':
+            office = 'Testreferat'
+
+        all_objects = chain(uni_objects, fin_objects, pos_objects, adv_members)
+        print(all_objects)
+        context = {
+            'uni_list': all_objects,
+            'pos': office
+        }
+        return render(request, 'intern_out.html', context)
+    else:
+        return render(request, 'stat_html/intern.html')
