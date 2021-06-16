8ace8d057432c8cfecb613a6e1dbdbb81d509821
Ruben Kraus <s80350@htw-dresden.de>
2021-06-16 10:55:59 +0200

    changed models __str__ to display correctly on admin

--- docs/implementation/Django_test/atool/models.py
+++ docs/implementation/Django_test/atool/models.py
@@ -15,13 +15,19 @@ class AdvisoryMember(models.Model):
     frg2 = models.CharField(max_length=260, null=True)
     frg3 = models.CharField(max_length=260, null=True)
     frg4 = models.CharField(max_length=260, null=True)
+    mehrheit = models.CharField(max_length=25, null=True)
+    beschluss = models.CharField(max_length=25, null=True)
+    beschlusstext = models.CharField(max_length=25, null=True)
+    beschlussgrund = models.CharField(max_length=25, null=True)
 
     class Meta:
         ordering = ['number']
 
     def __str__(self):
-        return self.flag, self.number, self.date, self.date, self.title, self.office, self.name, self.mail, self.text, \
-               self.frg1, self.frg2, self.frg3, self.frg4
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
+        self.flag, self.number, self.date, self.date, self.title, self.office, self.name, self.mail, self.text, \
+        self.frg1, self.frg2, self.frg3, self.frg4, self.mehrheit, self.beschluss, self.beschlusstext,
+        self.beschlussgrund)
 
 
 class Finance(models.Model):
@@ -36,13 +42,19 @@ class Finance(models.Model):
     reason = models.CharField(max_length=260, null=True)
     budget = models.CharField(max_length=260, null=True)
     suggestion = models.CharField(max_length=260, null=True)
+    mehrheit = models.CharField(max_length=25, null=True)
+    beschluss = models.CharField(max_length=25, null=True)
+    beschlusstext = models.CharField(max_length=25, null=True)
+    beschlussgrund = models.CharField(max_length=25, null=True)
 
     class Meta:
         ordering = ['number']
 
     def __str__(self):
-        return self.flag, self.number, self.date, self.date, self.title, self.office, self.name, self.mail, self.text, \
-               self.reason, self.budget, self.suggestion
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
+        self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, \
+        self.reason, self.budget, self.suggestion, self.mehrheit, self.beschluss, self.beschlusstext,
+        self.beschlussgrund)
 
 
 class Position(models.Model):
@@ -61,13 +73,18 @@ class Position(models.Model):
     frg_spez_1 = models.CharField(max_length=260, null=True)
     frg_spez_2 = models.CharField(max_length=260, null=True)
     frg_spez_3 = models.CharField(max_length=260, null=True)
+    mehrheit = models.CharField(max_length=25, null=True)
+    beschluss = models.CharField(max_length=25, null=True)
+    beschlusstext = models.CharField(max_length=25, null=True)
+    beschlussgrund = models.CharField(max_length=25, null=True)
 
     class Meta:
         ordering = ['number']
 
     def __str__(self):
-        return self.flag, self.number, self.date, self.date, self.title, self.office, self.name, self.mail, self.text, \
-               self.frg1, self.frg2, self.frg3, self.frg4, self.frg_spez_1, self.frg_spez_2, self.frg_spez_3
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
+            self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, self.frg1, self.frg2, self.frg3, self.frg4, self.frg_spez_1, self.frg_spez_2, self.frg_spez_3, self.mehrheit,
+            self.beschluss, self.beschlusstext, self.beschlussgrund)
 
 
 class Universall(models.Model):
@@ -81,10 +98,15 @@ class Universall(models.Model):
     text = models.CharField(max_length=260, null=True)
     reason = models.CharField(max_length=260, null=True)
     suggestion = models.CharField(max_length=260, null=True)
+    mehrheit = models.CharField(max_length=25, null=True)
+    beschluss = models.CharField(max_length=25, null=True)
+    beschlusstext = models.CharField(max_length=25, null=True)
+    beschlussgrund = models.CharField(max_length=25, null=True)
 
     class Meta:
         ordering = ['number']
 
     def __str__(self):
-        return self.flag, self.number, self.date, self.date, self.title, self.office, self.name, self.mail, self.text, \
-               self.reason, self.suggestion
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
+        self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, self.reason,
+        self.suggestion, self.mehrheit, self.beschluss, self.beschlusstext, self.beschlussgrund)
