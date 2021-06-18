4eee6219d7403ac52381ea3c332c9cec91cd3535
Ruben Kraus <s80350@htw-dresden.de>
2021-06-18 17:49:07 +0200

    ADded all urls for changing objects

--- docs/implementation/Django_test/atool/urls.py
+++ docs/implementation/Django_test/atool/urls.py
@@ -3,10 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('universall/', views.new_universall, name="universall"),
     path('finance/', views.new_finance, name="finance"),
     path('election_report/', views.new_position, name="election_report"),
     path('advisory_member/', views.new_advisory, name="advisory_member"),
     path('intern/', views.get_all_by_electioninput, name="intern"),
-    path('show/uni', views.change_universall, name="uni"),
+
+    path('show/universal', views.change_universall, name="change_uni"),
+    path('show/advisory', views.change_advisory, name="change_advi"),
+    path('show/position', views.change_position, name="change_posi"),
+    path('show/finance', views.change_finance, name="change_fin"),
+
 ]
