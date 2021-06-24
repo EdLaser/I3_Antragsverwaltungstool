16c44fb5c8f10ebe7a30512e248fd7d759fa9096
Ruben Kraus <s80350@htw-dresden.de>
2021-06-24 19:38:55 +0200

    Updated model and views because of new election input

--- docs/implementation/Django_test/Antragsverwaltungstool/models.py
+++ docs/implementation/Django_test/Antragsverwaltungstool/models.py
@@ -7,7 +7,7 @@ class AdvisoryMember(models.Model):
     number = models.CharField(max_length=12, primary_key=True, unique=True)
     date = models.DateField(default=django.utils.timezone.now)
     title = models.CharField(max_length=25, null=True)
-    office = models.CharField(max_length=50, null=True)
+    office = models.CharField(max_length=75, null=True)
     name = models.CharField(max_length=25, null=True)
     mail = models.EmailField(max_length=30, null=True)
     text = models.CharField(max_length=260, null=True)
@@ -15,6 +15,8 @@ class AdvisoryMember(models.Model):
     frg2 = models.CharField(max_length=260, null=True)
     frg3 = models.CharField(max_length=260, null=True)
     frg4 = models.CharField(max_length=260, null=True)
+    anlagen = models.CharField(max_length=260, null=True)
+    aenderung = models.CharField(max_length=260, null=True)
     mehrheit = models.CharField(max_length=25, null=True)
     beschluss = models.CharField(max_length=25, null=True)
     beschlusstext = models.CharField(max_length=25, null=True)
@@ -24,10 +26,10 @@ class AdvisoryMember(models.Model):
         ordering = ['number']
 
     def __str__(self):
-        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
-        self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, \
-        self.frg1, self.frg2, self.frg3, self.frg4, self.mehrheit, self.beschluss, self.beschlusstext,
-        self.beschlussgrund)
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
+            self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text,
+            self.frg1, self.frg2, self.frg3, self.frg4, self.anlagen, self.aenderung, self.mehrheit, self.beschluss,
+            self.beschlusstext, self.beschlussgrund)
 
 
 class Finance(models.Model):
@@ -35,13 +37,15 @@ class Finance(models.Model):
     number = models.CharField(max_length=12, primary_key=True, unique=True)
     date = models.DateField(default=django.utils.timezone.now)
     title = models.CharField(max_length=25, null=True)
-    office = models.CharField(max_length=50, null=True)
+    office = models.CharField(max_length=75, null=True)
     name = models.CharField(max_length=25, null=True)
     mail = models.EmailField(max_length=30, null=True)
     text = models.CharField(max_length=260, null=True)
     reason = models.CharField(max_length=260, null=True)
     budget = models.CharField(max_length=260, null=True)
     suggestion = models.CharField(max_length=260, null=True)
+    anlagen = models.CharField(max_length=260, null=True)
+    aenderung = models.CharField(max_length=260, null=True)
     mehrheit = models.CharField(max_length=25, null=True)
     beschluss = models.CharField(max_length=25, null=True)
     beschlusstext = models.CharField(max_length=25, null=True)
@@ -51,10 +55,10 @@ class Finance(models.Model):
         ordering = ['number']
 
     def __str__(self):
-        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
-        self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, \
-        self.reason, self.budget, self.suggestion, self.mehrheit, self.beschluss, self.beschlusstext,
-        self.beschlussgrund)
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
+            self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text,
+            self.reason, self.budget, self.suggestion, self.anlagen, self.aenderung, self.mehrheit, self.beschluss,
+            self.beschlusstext, self.beschlussgrund)
 
 
 class Position(models.Model):
@@ -62,7 +66,7 @@ class Position(models.Model):
     number = models.CharField(max_length=12, primary_key=True, unique=True)
     date = models.DateField(default=django.utils.timezone.now)
     title = models.CharField(max_length=25, null=True)
-    office = models.CharField(max_length=50, null=True)
+    office = models.CharField(max_length=75, null=True)
     name = models.CharField(max_length=25, null=True)
     mail = models.EmailField(max_length=30, null=True)
     text = models.CharField(max_length=260, null=True)
@@ -73,6 +77,8 @@ class Position(models.Model):
     frg_spez_1 = models.CharField(max_length=260, null=True)
     frg_spez_2 = models.CharField(max_length=260, null=True)
     frg_spez_3 = models.CharField(max_length=260, null=True)
+    anlagen = models.CharField(max_length=260, null=True)
+    aenderung = models.CharField(max_length=260, null=True)
     mehrheit = models.CharField(max_length=25, null=True)
     beschluss = models.CharField(max_length=25, null=True)
     beschlusstext = models.CharField(max_length=25, null=True)
@@ -82,9 +88,10 @@ class Position(models.Model):
         ordering = ['number']
 
     def __str__(self):
-        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
-            self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, self.frg1, self.frg2, self.frg3, self.frg4, self.frg_spez_1, self.frg_spez_2, self.frg_spez_3, self.mehrheit,
-            self.beschluss, self.beschlusstext, self.beschlussgrund)
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
+            self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, self.frg1,
+            self.frg2, self.frg3, self.frg4, self.frg_spez_1, self.frg_spez_2, self.frg_spez_3, self.anlagen,
+            self.aenderung, self.mehrheit, self.beschluss, self.beschlusstext, self.beschlussgrund)
 
 
 class Universall(models.Model):
@@ -92,12 +99,14 @@ class Universall(models.Model):
     number = models.CharField(max_length=12, primary_key=True, unique=True)
     date = models.DateField(default=django.utils.timezone.now)
     title = models.CharField(max_length=25, null=True)
-    office = models.CharField(max_length=50, null=True)
+    office = models.CharField(max_length=75, null=True)
     name = models.CharField(max_length=25, null=True)
     mail = models.EmailField(max_length=30, null=True)
     text = models.CharField(max_length=260, null=True)
     reason = models.CharField(max_length=260, null=True)
     suggestion = models.CharField(max_length=260, null=True)
+    anlagen = models.CharField(max_length=260, null=True)
+    aenderung = models.CharField(max_length=260, null=True)
     mehrheit = models.CharField(max_length=25, null=True)
     beschluss = models.CharField(max_length=25, null=True)
     beschlusstext = models.CharField(max_length=25, null=True)
@@ -107,6 +116,7 @@ class Universall(models.Model):
         ordering = ['number']
 
     def __str__(self):
-        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
-        self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, self.reason,
-        self.suggestion, self.mehrheit, self.beschluss, self.beschlusstext, self.beschlussgrund)
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s' % (
+            self.flag, self.number, self.date, self.title, self.office, self.name, self.mail, self.text, self.reason,
+            self.suggestion, self.anlagen, self.aenderung, self.mehrheit, self.beschluss, self.beschlusstext,
+            self.beschlussgrund)
