c6a1277126b7c713274091bc96036e7246c8b8b3
Ruben Kraus <s80350@htw-dresden.de>
2021-06-28 22:25:15 +0200

    added conduct to registered models

--- docs/implementation/Django_test/Antragsverwaltungstool/admin.py
+++ docs/implementation/Django_test/Antragsverwaltungstool/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import AdvisoryMember, Finance, Universall, Position
+from .models import AdvisoryMember, Finance, Universall, Position, Conduct
 
 # Register your models here.
 admin.site.register(AdvisoryMember)
 admin.site.register(Finance)
 admin.site.register(Universall)
 admin.site.register(Position)
+admin.site.register(Conduct)
