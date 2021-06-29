4cf1f0854df44897ffc2a9c1046f4f881a7a94e7
Ruben Kraus <s80350@htw-dresden.de>
2021-06-29 23:58:46 +0200

    added admin comments

--- docs/implementation/Django_test/Antragsverwaltungstool/admin.py
+++ docs/implementation/Django_test/Antragsverwaltungstool/admin.py
@@ -1,10 +1,16 @@
+""" Registers models on the admin Page. """
+
 from django.contrib import admin
 
 from .models import AdvisoryMember, Finance, Universall, Position, Conduct
 
-# Register your models here.
 admin.site.register(AdvisoryMember)
+""" Register the AdvisoryMember Model on admin page. """
 admin.site.register(Finance)
+""" Register the Finance Model on admin page. """
 admin.site.register(Universall)
+""" Register the Universall Model on admin page. """
 admin.site.register(Position)
+""" Register the Position Model on admin page. """
 admin.site.register(Conduct)
+""" Register the Conduct Model on admin page. """
