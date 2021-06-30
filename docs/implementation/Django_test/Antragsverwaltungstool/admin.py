21de9cf777c7ef8d5cdf251e61c2c6dc0379efd7
Ruben Kraus <s80350@htw-dresden.de>
2021-06-30 10:51:56 +0200

    registered model for number count of application

--- docs/implementation/Django_test/Antragsverwaltungstool/admin.py
+++ docs/implementation/Django_test/Antragsverwaltungstool/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from .models import AdvisoryMember, Finance, Universall, Position, Conduct
+from .models import AdvisoryMember, Finance, Universall, Position, Conduct, NumberCount
 
 admin.site.register(AdvisoryMember)
 """ Register the AdvisoryMember Model on admin page. """
@@ -14,3 +14,4 @@ admin.site.register(Position)
 """ Register the Position Model on admin page. """
 admin.site.register(Conduct)
 """ Register the Conduct Model on admin page. """
+admin.site.register(NumberCount)
